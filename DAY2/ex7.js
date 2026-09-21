00ce56d7590044f634dd7b8868b4b5ce4f72675c
abdellatif <abdelatif.yabahddou@gmail.com>
2026-09-21 10:49:54 +0000

    solved the seventh exercise of DAY 2 v2

--- DAY2/ex7.js
+++ DAY2/ex7.js
@@ -1,16 +1,12 @@
 function applyOperation(a, b, operation) {
     return operation(a, b);
 }
-function add(x, y) {
+const add = (x,y) => {
     return x + y;
 }
-function minus(x, y) {
+
+const minus = (x,y) => {
     return x - y;
 }
-function multiply(x, y) {
-    return x * y;
-}
-function divide(x, y) {
-    return x / y;
-}
-console.log(applyOperation(3, 3, divide));
+
+console.log(applyOperation(7, 3, add));
